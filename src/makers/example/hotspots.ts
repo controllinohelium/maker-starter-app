3ff278cc67ac6dfd57365aa444f2c2ee496f53a6
controllinohelium <86105933+controllinohelium@users.noreply.github.com>
2022-02-04 09:13:01 +0100

    Update hotspots.ts

--- src/makers/example/hotspots.ts
+++ src/makers/example/hotspots.ts
@@ -2,59 +2,21 @@ import HotspotIcon from './icon.svg'
 import { MakerHotspot } from '../hotspotMakerTypes'
 import ANTENNAS from './antennas'
 
-const ExampleHotspotBLE = {
-  name: 'Example Hotspot BLE',
-  icon: HotspotIcon,
-  onboardType: 'BLE',
-  translations: {
-    en: {
-      internal: [
-        {
-          title: '[title.1]',
-          body:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          button: '[next button - 1]',
-        },
-        {
-          title: '[title.2]',
-          body:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          button: '[next button - 2]',
-        },
-        {
-          title: '[title.3]',
-          body:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          button: '[finish button]',
-        },
-      ],
-    },
-    ja: {},
-    ko: {},
-    zh: {},
-  },
-  antenna: {
-    us: ANTENNAS.EXAMPLE_US,
-    default: ANTENNAS.EXAMPLE_EU,
-  },
-} as MakerHotspot
-
 const ExampleHotspotQR = {
-  name: 'Example Hotspot QR',
+  name: 'Controllino Hotspot QR',
   icon: HotspotIcon,
   onboardType: 'QR',
   translations: {
     en: {
-      externalOnboard: '[Your instructions here]',
+      externalOnboard: 'Please generate your QR code in the dashboard.',
     },
     ja: {},
     ko: {},
     zh: {},
   },
   antenna: {
-    us: ANTENNAS.EXAMPLE_US,
-    default: ANTENNAS.EXAMPLE_EU,
+    default: ANTENNAS.CTRL_EU,
   },
 } as MakerHotspot
 
-export default { ExampleHotspotBLE, ExampleHotspotQR }
+export default { ExampleHotspotQR }
